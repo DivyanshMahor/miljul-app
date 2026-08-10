7b119853aaf2e6fc584ad09f7c2a493b5b0ca094
Divyansh <divyanshmahor3@gmail.com>
2026-08-11 02:39:03 +0530

    2:40AM - Complete app

--- lib/feature/home/screen/meeting_room_screen.dart
+++ lib/feature/home/screen/meeting_room_screen.dart
@@ -1,11 +1,13 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:online_class/core/provider/join_meeting_provider.dart';
 import 'package:online_class/core/provider/meeting_room_provider.dart';
 import 'package:online_class/core/provider/new_meeting_provider.dart';
 import 'package:online_class/core/utils/colors.dart';
+import 'package:online_class/core/utils/utils.dart';
 import 'package:online_class/feature/home/screen/app_main_screen.dart';
 import 'package:online_class/feature/home/screen/waiting_approval_screen.dart';
 import 'package:online_class/route.dart';
@@ -32,7 +34,75 @@ class MeetingRoomScreen extends ConsumerStatefulWidget {
   ConsumerState<MeetingRoomScreen> createState() => _MeetingRoomScreenState();
 }
 
-class _MeetingRoomScreenState extends ConsumerState<MeetingRoomScreen> {
+class _MeetingRoomScreenState extends ConsumerState<MeetingRoomScreen>  {
+
+  //build mic/camera icons + remove button
+  Widget _buildUserStatusIcons(ZegoUIKitUser user){
+    return Row(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+
+        //show remove button nly if current user  is host
+        //and not trying to remove themselves
+        if(widget.isHost &&
+            user.id != FirebaseAuth.instance.currentUser?.uid) ...[
+          SizedBox(width: 8),
+          IconButton(onPressed: () => _showRemoveUserDialgo(user),
+
+            icon: Icon(Icons.remove_circle, color: Colors.red, size:20),
+          ),
+        ],
+
+        //mic status icon
+        ValueListenableBuilder<bool>(
+          valueListenable: user.microphone,
+          builder: (_,isMicOn,__) =>Icon(
+            isMicOn ? Icons.mic : Icons.mic_off,
+            color:Colors.white24,
+            size:20,
+          ),
+        ),
+        SizedBox(width: 8),
+
+        //Camera status icon
+        ValueListenableBuilder<bool>(
+          valueListenable: user.camera,
+          builder: (_,isCameraOn,__) =>Icon(
+            isCameraOn ? Icons.videocam : Icons.videocam_off,
+            color:Colors.white24,
+            size:20,
+          ),
+        ),
+      ],
+    );
+  }
+
+  void _showRemoveUserDialgo(ZegoUIKitUser user){
+    showDialog(context: context, builder: (_){
+      return AlertDialog(
+        title: Text("Remove User"),
+        content:  Text("Remove ${user.name} from the meeting"),
+        actions: [
+          IconButton(onPressed: Navigator.of(context).pop, icon: Text("Cancel")),
+          TextButton(
+            onPressed:(){
+        Navigator.of(context).pop();
+      ref.read(meetingRoomProvider.notifier).removeUser(user);
+      showAppSnackbar(
+          context: context,
+          type: SnackbarType.success,
+          description: "${user.name} removem from the meeting"
+      );
+      },
+
+            child: Text("Remove"),
+          ),
+        ],
+      );
+    });
+  }
+
+
   @override
   Widget build(BuildContext context) {
     final user = FirebaseAuth.instance.currentUser;
@@ -147,7 +217,7 @@ class _MeetingRoomScreenState extends ConsumerState<MeetingRoomScreen> {
                       .doc(widget.roomId)
                       .get();
                   final hostId = roomSnapshot['hostId'] ?? "";
-                  final cuurentUsers = ZegoUIKit().getAllUsers();
+                  final currentUsers = ZegoUIKit().getAllUsers();
                   showModalBottomSheet(
                       context: context,
                       backgroundColor: bottomSheetColor,
@@ -157,9 +227,16 @@ class _MeetingRoomScreenState extends ConsumerState<MeetingRoomScreen> {
                       ),
                       ),
                       builder: (_){
-                    return ;
+                    return _buildMemberList(hostId, currentUsers) ;
                   } );
-                }, icon: Icon(Icons.group, color: Colors.white,),),
+                }, icon: Icon(Icons.group, color: Colors.white,),
+                      ),
+                   
+                   //show total participant count on top
+                      Positioned(child: Text(ZegoUIKit().getAllUsers().length.toString(),
+                      style: TextStyle(color: Colors.white),
+                      ),),
+
                     ],
                   ),
                 ]
@@ -219,8 +296,105 @@ class _MeetingRoomScreenState extends ConsumerState<MeetingRoomScreen> {
           Divider(color: Colors.black26),
           SizedBox(height: 5),
 
+          //list of participants
+          Expanded(
+            child: StreamBuilder(
+                stream: ZegoUIKit().getUserListStream(),
+                builder: (_, snapshot) {
+            final  participants = snapshot.data ?? [];
+            if (participants.isEmpty){
+              return const Center(
+                child: Text("No participants yet",
+                  style: TextStyle(color: Colors.white),
+                ),
+              );
+            }
+            return ListView.builder
+              (
+              itemCount: participants.length,
+                itemBuilder: (context, index) {
+                final user = participants[index];
+                final isHost = user.id == hostId;
+                return ListTile(
+
+                  //show profile picture of all user (from firestore)
+                  leading: StreamBuilder<DocumentSnapshot>(
+                      stream: FirebaseFirestore.instance
+                  .collection("users")
+                          .doc(user.id)
+                          .snapshots(),
+                      builder : (context, userSnapshot){
+                        String? photoUrl;
+                        if(userSnapshot.hasData &&
+                        userSnapshot.data!.exists){
+                          final userData = userSnapshot.data!.data() as Map<String, dynamic>?;
+                          photoUrl = userData?['photoUrl'];
+
+
+
+                        }  //fallback for current user
+                        if(
+                        photoUrl == null
+                            &&
+                            user.id == FirebaseAuth.instance.currentUser?.uid
+                        ){
+
+                          photoUrl = FirebaseAuth.instance.currentUser?.photoURL;
+                        }
+                           return CircleAvatar(
+                          backgroundColor: Colors.white,
+                          radius: 20,
+                          backgroundImage: photoUrl != null && photoUrl.isNotEmpty ?
+                          NetworkImage(photoUrl):NetworkImage("https://cdn.pixabay.com/photo/2023/02/18/icon-7797704_640.png"),
+                        );
+                }
+                  ) ,
+                 
+                  //show username with (you) or host
+                  title: Text(_getUserDisplayName(user, isHost),
+                  style: TextStyle(
+                    color: Colors.white,
+                  fontWeight: isHost ?
+                  FontWeight.w600
+                      : FontWeight.normal,
+                  ),
+                  ),
+                  //mic, Camera, remove button
+                  trailing: _buildUserStatusIcons(user),
+                );
+
+                } );
+          },
+
+          ),
+          ),
+
+
         ],
       ),
     );
   }
+
+  //helper: add (you) and host to names
+String _getUserDisplayName(ZegoUIKitUser user, bool isHost) {
+
+    final currentUserId = FirebaseAuth.instance.currentUser?.uid;
+    final isCurrentUser = user.id == currentUserId;
+    String displayName = user.name;
+    if(isCurrentUser) displayName += "(You)";
+    if(isHost) displayName += "(Host)";
+    return displayName;
+}
+
+//display to confirm removing a user fro meeting (only for host can remove),
+
+//display to confirm removing a user from meeting (only for host can remove),
+
+
+
+
+
+
+
+
 }
